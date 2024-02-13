2c15cbc19fc41e84b683c57f0f297a1a97d21144
ikramuzzaman455173 <jakaria455173@gmail.com>
2024-02-13 15:42:27 +0600

    gram bazar initial setup part completes

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className='text-center font-bold text-h1 border-2 mt-5'>This Is Our Home Page:)</div>
+    </>
+  )
+}
+
+export default Home
